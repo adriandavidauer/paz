d8feaa534533db138012ecac0d36ed2533396e04
Adrian Auer <adrian.auer@dfki.de>
2025-07-21 11:32:44 +0000

    intermediate push for downoading dataset in generator

--- examples/visual_voice_activity_detection/vvad_lrs3_dataset.py
+++ examples/visual_voice_activity_detection/vvad_lrs3_dataset.py
@@ -1,20 +1,31 @@
+'''
+VVAD-LRS3 Dataset Generator
+'''
+
+# System imports
 import os
 import h5py
 import random
 import numpy as np
 import math
 from typing import Literal, get_args
-
+# 3rd party imports
 from paz.datasets.utils import get_class_names
+import keras
+# local imports
 from generator import Generator
+# end file header
+__author__      = 'Adrian Auer'
 
-Reduction_Method = Literal['cut', 'reduce']
 
+REDUCTION_METHODS = ['cut', 'reduce']
+DATA_VARIANTS = ['face_images', 'lip_images', 'face_features', 'lip_features']
+URL= "https://www.kaggle.com/api/v1/datasets/download/adrianlubitz/vvadlrs3"
 
 class VvadLrs3Dataset(Generator):
     """Class for generating the VVAD-LRS3 dataset.
     # Arguments
-        path: String. Full path to vvadlrs3_faceImages_small.h5 file.
+        path: String. Full path to vvadlrs3 folder containing 'faceImages_small.h5', 'lipImages.h5', 'faceFeatures.h5' and 'lipFeatures.h5' files.
         split: String. Valid option contain 'train', 'validation' or 'test'.
         validation_split: Float. Percentage of the dataset to be used for validation (valid options between 0.0 to 1.0). Set
             to 0.0 to disable.
@@ -28,13 +39,14 @@ class VvadLrs3Dataset(Generator):
             the clip.
         reduced_length: Float. The length of the video after the reduction_method is applied. Choose None if you
             want to keep the original size. None is the default
+        data_variant: String. The type of data to be used. Valid options are 'face_images', 'lip_images', 'face_features', 'lip_features'.
 
     # References
         -[VVAD-LRS3](https://www.kaggle.com/datasets/adrianlubitz/vvadlrs3)
     """
     def __init__(
-            self, path='.keras/paz/datasets', split='train', validation_split=0.2, test_split=0.1, testing=False,
-            evaluating=False, reduction_method: Reduction_Method = 'cut', reduced_length=None):
+            self, path='~/.keras/paz/datasets', split='train', validation_split=0.2, test_split=0.1, testing=False,
+            evaluating=False, reduction_method: str = 'cut', reduced_length=None, data_variant:str='face_images'):
         if split != 'train' and split != 'validation' and split != 'test':
             raise ValueError('Invalid split name')
         if validation_split < 0.0 or validation_split > 1.0:
@@ -43,10 +55,36 @@ class VvadLrs3Dataset(Generator):
             raise ValueError('Invalid test split')
         if validation_split + test_split > 1.0:
             raise ValueError('The sum of val_split and test_split must be less than 1.0')
-        options = get_args(Reduction_Method)
-        assert reduction_method in options, f"'{reduction_method}' is not in {options}"
+        if data_variant not in DATA_VARIANTS:
+            raise ValueError(f"Invalid data_variant '{data_variant}'. Valid options are {DATA_VARIANTS}")
+        if reduction_method not in REDUCTION_METHODS:
+            raise ValueError(f"Invalid reduction_method '{reduction_method}'. Valid options are {REDUCTION_METHODS}")
 
-        path = os.path.join(path, 'vvadlrs3_faceImages_small.h5')
+        # TODO: Check downloading with different paths + remove print statements
+        print("Downloading VVAD-LRS3 dataset...")
+        path = os.path.expanduser(path)
+        print("Data will be downloaded to:", path)
+        extract_dir = os.path.join(path, "vvadlrs3_extracted") 
+        print("Data extracted path:", extract_dir)
+        if not os.path.exists(extract_dir):
+            data_path = keras.utils.get_file(origin=URL, fname="vvadlrs3.zip", cache_dir=path, extract=True)
+        else:
+            print("Already extracted, skipping extraction.")
+        # print the content of the data_path
+        print("Data path:", data_path)
+        
+        print("Files in data extracted path:", os.listdir(extract_dir))
+
+        if data_variant == 'face_images':
+            path = os.path.join(extract_dir, 'faceImages_small.h5')
+        elif data_variant == 'lip_images':
+            path = os.path.join(extract_dir, 'lipImages.h5')
+        elif data_variant == 'face_features':
+            path = os.path.join(extract_dir, 'faceFeatures.h5')
+        elif data_variant == 'lip_features':
+            path = os.path.join(extract_dir, 'lipFeatures.h5')
+        print("Using dataset path:", path)
+            # path = os.path.join(path, 'vvadlrs3_faceImages_small.h5')
 
         class_names = get_class_names('VVAD_LRS3')
 
@@ -207,3 +245,12 @@ class VvadLrs3Dataset(Generator):
         elif self.split == 'test':
             print('total_size_test', self.total_dataset_size)
             return int(self.test_split * 0.5 * self.total_dataset_size) * 2
+
+if __name__ == '__main__':
+    # Example usage
+    dataset = VvadLrs3Dataset(path='~/.keras/paz/datasets', split='train', validation_split=0.2, test_split=0.1, testing=False,
+                              evaluating=False, reduction_method='cut', reduced_length=None, data_variant='face_images')
+    for sample, label in dataset():
+        print(sample.shape, label)
+        break  # Just to show the first sample
+    print("Number of classes:", dataset.num_classes)
